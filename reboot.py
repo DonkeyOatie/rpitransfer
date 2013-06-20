c0232e247aa5c4b5e7aac800c60a60b538877ac8
nkhumphreys <nathan@nkhumphreys.co.uk>
2013-06-20 19:53:27 +0100

    added wifi test

--- reboot.py
+++ reboot.py
@@ -2,9 +2,17 @@
 
 from subprocess import call
 from time import sleep
+import urllib2
 
 with open("reboot_nums.txt", "a") as reboot_file:
     reboot_file.write("1 ")
+
+with open("wifi_test.txt", "a") as wifi_file:
+    try:
+        response=urllib2.urlopen("http://74.125.113.99", timeout=1)
+        wifi_file.write("pass ")
+    except urllib2.URLError:
+        wifi_file.write("fail ")
 
 #sleep to give me time to remove this when I need to
 sleep(120)
