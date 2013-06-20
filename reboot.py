f9867df67943aca5ccb4d3f79317939d31446a1d
nkhumphreys <nkhumphreys@gmail.com>
2013-06-20 18:50:06 +0000

    updated

--- reboot.py
+++ reboot.py
@@ -1,10 +1,11 @@
+#! /usr/bin/python
+
 from subprocess import call
 from time import sleep
 
 with open("reboot_nums.txt", "a") as reboot_file:
-    num = int(reboot_file.readline())
-    num++
-    reboot_file.write(num)
+    reboot_file.write("1 ")
 
-sleep(60)
+#sleep to give me time to remove this when I need to
+sleep(120)
 call(["reboot"])
